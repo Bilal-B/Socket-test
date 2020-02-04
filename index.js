4094904b64a1b5ee6efa8ce7ee7448392773fd19
Mefyros <bilal.beldjilali@netcosports.com>
2020-02-04 10:45:24 +0100

    add of socket handler

---
+++ index.js
@@ -0,0 +1,76 @@
+const moment = require("moment")
+const WebSocket = require('ws');
+
+function connectSDDP() {
+  var outletObject = {
+    OutletKeyService: {
+      // replace below {outletid} value with your unique ID provided to you by our Client/Account Services 
+      outletid: '7vzjbeh98kgm12i8frsfl25x1'
+    }
+  };
+  // initialize WebSocket connection
+  var connection = new WebSocket('wss://content.performgroup.io');
+  connection.onopen = function (event) {
+    // when connection is opened, send outlet data to get authorized
+    connection.send(JSON.stringify({
+      outlet: outletObject
+    }));
+  };
+  connection.onmessage = (msg) => {
+    var msg = JSON.parse(msg.data);
+    // first time subscription sending for particular fixtureUuid
+    // based on response from outlet plugin
+    if (msg.outlet && msg.outlet.msg && msg.outlet.msg === 'is_authorised') {
+      console.log('subscribe to a fixture' );
+      subscriptionObj = {
+        "name": "subscribe",
+      // required.
+        "feed": ['matchEvent'],
+      // required. Value: Feed name you subscribe to, for example 'matchEvent' 'matchEventStats' 'stats' (replace feedName)
+        "fixtureUuid": "ddsn6eyqw3t3j6h5vukrlvckq",
+      // required. Value: Unique ID (UUID) of a fixture you want to subscribe to (eg c359t8yjkrvm9oyzm80pgjemt - replace uuid). Get UUIDs from the Tournament Schedule feed
+        "optaId": true
+      // optional (include if you want Opta IDs). Value: true|false (Opta IDs are not returned if you specify "optaId": false)
+      }
+      // send subscription request
+      connection.send(JSON.stringify({
+        content: subscriptionObj
+      }));
+    }
+    // do something with received match data
+    console.log(msg);
+  }
+}
+
+connectSDDP()
+
+"http://api.performfeeds.com/soccerdata/match/7vzjbeh98kgm12i8frsfl25x1?_rt=b&_fmt=json&_dlt=2020-02-03T010:00:00%2000:00"
+
+var match = [
+    {
+        "id": "ddsn6eyqw3t3j6h5vukrlvckq",
+        "coverageLevel": "13",
+        "date": "2020-10-04Z",
+        "time": "20:30:00Z",
+        "lastUpdated": "2020-02-04T04:30:19Z",
+        "description": "Sporting KC vs New York City",
+        "sport": {
+          "id": "289u5typ3vp4ifwh5thalohmq",
+          "name": "Soccer"
+        },
+        "ruleset": {
+          "id": "79plas4983031idr6vw83nuel",
+          "name": "Men"
+        },
+        "competition": {
+          "id": "287tckirbfj9nb8ar2k9r60vn",
+          "name": "MLS",
+          "competitionCode": "MLS",
+          "competitionFormat": "Domestic league",
+          "country": {
+            "id": "7hr2f89v44y65dyu9k92vprwn",
+            "name": "USA"
+          }
+        },
+    }
+]
